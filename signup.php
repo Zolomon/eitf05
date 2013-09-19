b84c35ba3cfb3ef3c49fa2f656fd4224a90a1450
Zolomon <zolomon@gmail.com>
2013-09-19 14:59:18 +0200

    Minor change

--- signup.php
+++ signup.php
@@ -20,27 +20,23 @@
         </div>
         <div class="form-group">
           <label for="exampleInputEmail1">First name</label>
-          <input type="username" class="form-control" id="username" placeholder="Enter user name">
-        </div>
-        <div class="form-group">
+          <input type="username" class="form-inline" id="username" placeholder="Enter user name">
           <label for="exampleInputEmail1">Sur name</label>
-          <input type="username" class="form-control" id="username" placeholder="Enter user name">
+          <input type="username" class="form-inline" id="username" placeholder="Enter user name">
         </div>
+        <!-- <div class="form-group">
+        </div> -->
         <div class="form-group">
           <label for="exampleInputEmail1">Home Address</label>
           <input type="username" class="form-control" id="username" placeholder="Enter user name">
         </div>
         <div class="form-group">
           <label for="exampleInputEmail1">Zip code</label>
-          <input type="username" class="form-control" id="username" placeholder="Enter user name">
-        </div>
-        <div class="form-group">
+          <input type="username" class="form-inline" id="username" placeholder="Enter user name">
           <label for="exampleInputEmail1">City</label>
-          <input type="username" class="form-control" id="username" placeholder="Enter user name">
-        </div>
-        <div class="form-group">
+          <input type="username" class="form-inline" id="username" placeholder="Enter user name">
           <label for="exampleInputEmail1">Country</label>
-          <input type="username" class="form-control" id="username" placeholder="Enter user name">
+          <input type="username" class="form-inline" id="username" placeholder="Enter user name">
         </div>
         <button type="submit" class="btn btn-default">Submit</button>
       </form>
@@ -51,5 +47,6 @@
 <?php 
 
 //$salt = uniqid(mt_rand(), true);
+//$password = hash_hmac('sha512', $password . $salt, $sitewide_key);
 
 ?>
