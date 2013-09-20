2d9a2b0321cd0fd402d05067450d602adec27bc7
Bengt Ericsson <zolomon@gmail.com>
2013-09-20 12:07:25 +0200

    fix indentation

--- signup.php
+++ signup.php
@@ -1,73 +1,73 @@
 <div class="container">
 	<div class="row">
-			<form class="form-horizontal" role="form" action="index.php" method="post">
-				<div class="form-group">
-					<label for="username" class="col-lg-2 control-label">Username</label>
-					<div class="col-lg-4">
-						<input type="text" class="form-control" id="username" name="username" placeholder="Enter a user name">
-					</div>          
-				</div>
+		<form class="form-horizontal" role="form" action="index.php" method="post">
+			<div class="form-group">
+				<label for="username" class="col-lg-2 control-label">Username</label>
+				<div class="col-lg-4">
+					<input type="text" class="form-control" id="username" name="username" placeholder="Enter a user name">
+				</div>          
+			</div>
 
-				<div class="form-group">
-					<label for="password" class="col-lg-2 control-label">Password</label>
-					<div class="col-md-4">
-						<input type="password" class="form-control" id="password" name="password" placeholder="Password">  
-					</div>
+			<div class="form-group">
+				<label for="password" class="col-lg-2 control-label">Password</label>
+				<div class="col-md-4">
+					<input type="password" class="form-control" id="password" name="password" placeholder="Password">  
 				</div>
-				<div class="form-group">
-					<label for="verifyPassword" class="col-lg-2 control-label">Verify Password</label>
-					<div class="col-md-4">
-						<input type="password" class="form-control" id="verifyPassword" name="verifyPassword" placeholder="Verify Password">
-					</div>          
-				</div>
-					
-				<div class="form-group">
-					<label for="exampleInputEmail1" class="col-lg-2 control-label">First name</label>
-					<div class="col-md-4">
-						<input type="text" class="form-control" id="firstname" name="firstname" placeholder="Enter first name">
-					</div>
-					<label for="exampleInputEmail1" class="col-lg-2 control-label">Sur name</label>
-					<div class="col-md-4">
-						<input type="text" class="form-control" id="surname" name="surname" placeholder="Enter your sur name">
-					</div>
-				</div> 
+			</div>
+			<div class="form-group">
+				<label for="verifyPassword" class="col-lg-2 control-label">Verify Password</label>
+				<div class="col-md-4">
+					<input type="password" class="form-control" id="verifyPassword" name="verifyPassword" placeholder="Verify Password">
+				</div>          
+			</div>
 				
-				<div class="form-group">
-					<label for="exampleInputEmail1" class="col-lg-2 control-label">Home Address</label>
-					<div class="col-md-10">
-						<input type="text" class="form-control" id="homeaddress" name="homeaddress" placeholder="Enter your home address">
-					</div>
+			<div class="form-group">
+				<label for="exampleInputEmail1" class="col-lg-2 control-label">First name</label>
+				<div class="col-md-4">
+					<input type="text" class="form-control" id="firstname" name="firstname" placeholder="Enter first name">
 				</div>
-
-				<div class="form-group">
-					<label for="exampleInputEmail1" class="col-lg-2 control-label">Zip code</label>
-					<div class="col-md-4">
-						<input type="text" class="form-control" id="zipcode" name="zipcode" placeholder="Enter your zip code">
-					</div>
+				<label for="exampleInputEmail1" class="col-lg-2 control-label">Sur name</label>
+				<div class="col-md-4">
+					<input type="text" class="form-control" id="surname" name="surname" placeholder="Enter your sur name">
 				</div>
-
-				<div class="form-group">
-					<label for="exampleInputEmail1" class="col-lg-2 control-label">City</label>
-					<div class="col-md-4">
-						<input type="text" class="form-control" id="city" name="city" placeholder="Enter the city your home address resides in">
-					</div>
+			</div> 
+			
+			<div class="form-group">
+				<label for="exampleInputEmail1" class="col-lg-2 control-label">Home Address</label>
+				<div class="col-md-10">
+					<input type="text" class="form-control" id="homeaddress" name="homeaddress" placeholder="Enter your home address">
 				</div>
+			</div>
 
-				<div class="form-group">
-					<label for="exampleInputEmail1" class="col-lg-2 control-label">Country</label>
-					<div class="col-md-4">
-						<input type="text" class="form-control" id="country" name="country" placeholder="Enter the country you live in">
-					</div>
+			<div class="form-group">
+				<label for="exampleInputEmail1" class="col-lg-2 control-label">Zip code</label>
+				<div class="col-md-4">
+					<input type="text" class="form-control" id="zipcode" name="zipcode" placeholder="Enter your zip code">
 				</div>
+			</div>
 
-				<div class="form-group">
-					<div class="col-lg-offset-2 col-lg-10">
-						<button type="submit" class="btn-lg btn-primary">Sign up</button>
-					</div>
+			<div class="form-group">
+				<label for="exampleInputEmail1" class="col-lg-2 control-label">City</label>
+				<div class="col-md-4">
+					<input type="text" class="form-control" id="city" name="city" placeholder="Enter the city your home address resides in">
 				</div>
+			</div>
 
-			</form>
-		</div>
+			<div class="form-group">
+				<label for="exampleInputEmail1" class="col-lg-2 control-label">Country</label>
+				<div class="col-md-4">
+					<input type="text" class="form-control" id="country" name="country" placeholder="Enter the country you live in">
+				</div>
+			</div>
+
+			<div class="form-group">
+				<div class="col-lg-offset-2 col-lg-10">
+					<button type="submit" class="btn-lg btn-primary">Sign up</button>
+				</div>
+			</div>
+
+		</form>
+	</div>
 </div>
 
 <?php 
