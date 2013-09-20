72227e174de1229e5a47d61bb82c8d9e5f74b8bc
Bengt Ericsson <zolomon@gmail.com>
2013-09-20 12:12:37 +0200

    Removed sensitive info from github

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+include 'config.php'
+
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+
+include 'header.php';
+
+include 'content.php';
+
+include 'footer.php';
+
+mysql_close($link);
+
+?>
