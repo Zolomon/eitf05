bbb9db2ed2b25a784663048ea48fea77b4d20c7e
Zolomon <zolomon@gmail.com>
2013-09-25 14:03:06 +0200

    fixed index

--- index.php
+++ index.php
@@ -2,10 +2,6 @@
 
 include 'config.php';
 
-if (!$link) {
-	die('Could not connect: ' . mysqli_error());
-}
-
 include 'session.php';		// Create a new session if none already exist, must be called before <html> since it's sent with the HTTP headers
 
 include 'header.php';
