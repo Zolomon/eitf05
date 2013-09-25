a66af5df9a0bc5580efa9603365b2033b6717ef5
prizescu <ada07pr1@student.lu.se>
2013-09-25 12:13:23 +0200

    Added code in shop

--- content/shop.php
+++ content/shop.php
@@ -1,5 +1,35 @@
 <?php
+/*
+function DisplayItems($stmt) 
+{
+	$i = 0;
+	while (mysqli_stmt_fetch($stmt)) {
+		if (i%3 == 0) {
+			echo "<div class='row'>";
+		}
+		echo "<div class='col-md-4'>$name \$$price[3]:<br><br>$description[2]</div>";
+			
+		if (i%3 == 2) {
+			echo "</div>";
+		}
+		$i = $i + 1;
+	}
+}
+
+function UpdateCurrPageNbr(){
+	
+}
+*/
+$shop_page = 1;
+$stmta = mysqli_prepare($link, "SELECT name,decription,price FROM items --LIMIT ((?-1)*15),15");
+mysqli_stmt_bind_param($stmta, 'i', $shop_page);
+/*mysqli_stmt_execute($stmta);
+mysqli_stmt_bind_result($stmta, $name, $description, $price);*/
+
+
+//DisplayItems($items)
 
 
 
+//'($_GET['shop_page']-1)*15'
 ?>
