05e4256df9db8ff64a7ca5b34ca1c3ff856f2b9d
prizescu <ada07pr1@student.lu.se>
2013-09-30 22:05:16 +0200

    Count and sum are now correct

--- content/cart.php
+++ content/cart.php
@@ -16,52 +16,74 @@ function display_items(&$stmt, &$db)
 							<th>Count</th>
 							<th>Remove</th>
 						</tr>
-						</thead>
+					</thead>
 EOT;
+	$already_in_list = array();
+	$total_sum = 0;
 	while ($result = $stmt->fetch(PDO::FETCH_ASSOC)){
 			$item_id = $result['item_id'];
 			if ($stmt2 = $db->prepare("SELECT id, name, description, price FROM items WHERE id=:item_id;")) {
 				$stmt2->bindParam(':item_id', $item_id, PDO::PARAM_INT);
 				$stmt2->execute();
 
-				while ($result = $stmt2	->fetch(PDO::FETCH_ASSOC)) {
-					$name = htmlspecialchars($result['name']); // protect against XSS
-					
-					if(strlen($name) > 30){
-						$name = " title=\"" . $name . "\">" . substr($name, 0, 28) . "..."; // protect against XSS
-					} else {
-						$name = ">" . $name;
-					}
+				if (!array_key_exists($item_id, $already_in_list)){
+					$already_in_list[$item_id] = 1;
+					$count_s = $db->prepare("SELECT COUNT(id) AS count FROM cart WHERE item_id=:item_id;");
+					$count_s->bindParam(':item_id', $item_id, PDO::PARAM_INT);
+					$count_s->execute();
+					$cres = $count_s->fetch(PDO::FETCH_ASSOC);
+					$count = $cres['count'];
+					while ($result = $stmt2->fetch(PDO::FETCH_ASSOC)) {
+						$name = htmlspecialchars($result['name']); // protect against XSS
+						
+						if(strlen($name) > 30){
+							$name = " title=\"" . $name . "\">" . substr($name, 0, 28) . "..."; // protect against XSS
+						} else {
+							$name = ">" . $name;
+						}
 
-					$description = htmlspecialchars($result['description']);
-					
-					if(strlen($description) > 100){
-						$description = " title=\"" . $description . "\">" . substr($description, 0, 98) . "..."; // protect against XSS
-					} else {
-						$description = ">" . $description;
-					}
+						$description = htmlspecialchars($result['description']);
+						
+						if(strlen($description) > 100){
+							$description = " title=\"" . $description . "\">" . substr($description, 0, 98) . "..."; // protect against XSS
+						} else {
+							$description = ">" . $description;
+						}
 
-							//<td><div class="col-sm-12"><input type="text" class="form-control input-sm" name="count_\${$result['id']}" value="1"></div></td>
-							//<td><input type="checkbox" name="count[]" value="item_\${$result['id']}" /></td>
-					$count = 1; //FIXA!!
-					$sum = $result['price'] * $count; //Make float!
-
-					echo <<<EOT
-						<tr>
-							<td>{$result['id']}</td>
-							<td$name</td>
-							<td$description</td>
-							<td>\${$result['price']}</td>
-							<td>$sum</td>
-							<td><div class="col-sm-12"><input type="text" class="form-control input-sm" name="count_\${$result['id']}" value="$count"></div></td>
-							<td><button type="submit" class="btn-sm btn-danger">X</button></td>
-						</tr>
+								//<td><div class="col-sm-12"><input type="text" class="form-control input-sm" name="count_\${$result['id']}" value="1"></div></td>
+								//<td><input type="checkbox" name="count[]" value="item_\${$result['id']}" /></td>
+						$sum = $result['price'] * $count;
+						$total_sum = $total_sum + $sum;
+						$sum = number_format((float)$sum, 2, '.', '');
+						echo <<<EOT
+							<tr>
+								<td>{$result['id']}</td>
+								<td$name</td>
+								<td$description</td>
+								<td>\${$result['price']}</td>
+								<td>\$$sum</td>
+								<td><div class="col-sm-12"><input type="text" class="form-control input-sm" name="count_\${$result['id']}" value="$count"></div></td>
+								<td><button type="submit" class="btn-sm btn-danger">X</button></td>
+							</tr>
 EOT;
-					//printf("<div class='col-md-4'>%s \$%s:<br><br>%s</div>", $result['name'], $result['price'], $result['description']);
+						//printf("<div class='col-md-4'>%s \$%s:<br><br>%s</div>", $result['name'], $result['price'], $result['description']);
+					}
 				}
 			}
 		}
+		$total_sum = number_format((float)$total_sum, 2, '.', '');
 		echo <<<EOT
+					<tfoot>
+						<tr>
+							<td></td>
+							<td></td>
+							<td></td>
+							<th>Total:</th>
+							<th>\$$total_sum</th>
+							<td></td>
+							<td></td>
+						</tr>
+					</tfoot>
 				</table>
 				<div class="form-group">
 					<div class="col-lg-offset-10 col-lg-10">
