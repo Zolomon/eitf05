5cb185c8b8564349bda8f9e2fc047e4a96a8f899
prizescu <ada07pr1@student.lu.se>
2013-10-02 17:10:28 +0200

    ssl

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
+
+/* Force https */
+$protocol = 'http';
+if (array_key_exists('HTTPS', $_SERVER) && $_SERVER['HTTPS'] === 'on') {
+	$protocol = 'https';
+}
+if ($protocol != 'https') {
+	$actual_link = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+	header( "HTTP/1.1 301 Moved Permanently" );
+	header( "Location: $actual_link" );
+	exit();
+}
+
 try {
 
 	include 'config.php';
@@ -14,4 +27,5 @@ try {
 } catch (PDOException $ex) {
 	echo $ex->getMessage();
 }
+
 ?>
